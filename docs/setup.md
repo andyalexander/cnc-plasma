89c799c252a8f887a9bc8a4e71a285ee83335c82
Andy Alexander <code@homeemail.me.uk>
2025-11-30 14:31:55 +0000

    fix: updates

--- docs/setup.md
+++ docs/setup.md
@@ -3,6 +3,28 @@
 * MESA manual https://www.mesanet.com/pdf/parallel/7i95tman.pdf
 
 
+### Latency
+
+Taken from: https://forum.linuxcnc.org/27-driver-boards/52849-error-finishing-read
+
+
+* Run `latency-histogram --nobase --sbinsize 1000`
+
+* Disable all power management options in the BIOS setup. This include turbo modes, EIST, Cstates > C1, Cool&Quiet etc, basically anything that causes the CPU to change speeds dynamically or sleep.
+
+* Also in the BIOS you should disable hyperthreading and any management engine  related  options that affect the network (like AMT)
+
+* If your PC has an Intel Ethernet chip make sure you disable IRQ coalescing (man hm2_eth)
+
+* Pinging the 7I96S will give you some idea of network latency
+```
+ping -i .2 -c 4 10.10.10.10
+sudo chrt 99 ping -i .001 -q 10.10.10.10
+```
+
+ (let the last command run for a few minutes and
+then stop it with a control C, it will print timing statistics)
+
 
 
 ## Debian config
@@ -15,10 +37,14 @@
 ## Torch height controller
 
 * Manual: https://www.mesanet.com/pdf/analog/thcad2man.pdf
-
+* If you are using QTPlasmac - the offset and scale parameters are in the parameters screen in the UI, the ones in he HAL file don't matter
+* If using ohmic probe, you must check `ohmic probe enable` in the GUI
 
 ## Ohmic sensor
 
 https://linuxcnc.org/docs/2.9/html/plasma/plasma-cnc-primer.html#_initial_height_sensing
 
 
+## QTPlasmac hints
+* On the 'settings' page make sure that 'KB shortcuts' is enabled if you want to be able to use keyboard jogging
+* 
--- utils/thcad_calc.py
+++ utils/thcad_calc.py
@@ -1,8 +1,11 @@
-boards = [{'freq_zero': 100.4, 'freq_full': 904.9}, {'freq_zero': 101.2, 'freq_full': 904.4}]
+boards = [{'freq_zero': 100.4, 'freq_full': 904.9, 'volt_full':5 ,'freq_div':32, 'plasma_divider_ratio':50},
+          {'freq_zero': 101.2, 'freq_full': 904.4, 'volt_full':5,'freq_div':32,'plasma_divider_ratio':1},
+          ]
 
-volt_full = 5  # or 5 (W1=down) or 10 (W1=up)
-freq_div = 32  # or 32 or 64 or 128, 32 recommended in QTPlasmac docs
-plasma_divider_ratio = 50  # default is 50:1; better is 20:1
+# VOLT RANGE: 5 (W1=down) or 10 (W1=up)
+# freq_div = 32 or 64 or 128, 32 recommended in QTPlasmac docs
+
+#plasma_divider_ratio: default is 50:1; better is 20:1, if using 50:1 use a 5v scale, if 20:1 use 10v
 encoder_max_freq = 3e6  # 3 MHz
 encoder_implied_scale = 1 / encoder_max_freq
 
@@ -10,14 +13,15 @@ max_torch_voltage = 200
 
 for i, board in enumerate(boards):
     print(f"Board {i + 1}: F0={board['freq_zero']} MHz, FFS={board['freq_full']} MHz")
-    scale = volt_full * freq_div * plasma_divider_ratio / (1000 * (board['freq_full'] - board['freq_zero']))
-    offset = 1000 * board['freq_zero'] / freq_div
+    scale = board['volt_full'] * board['freq_div'] * board['plasma_divider_ratio'] / (1000 * (board['freq_full'] - board['freq_zero']))
+    offset = 1000 * board['freq_zero'] / board['freq_div']
 
-    print(f"Scale: {scale:.8f}, Offset: {offset:.8f}")
+    print(f"Scale: {scale:.6f}, Offset: {offset:.3f}")
 
     print(f"Freq at max voltage: {(max_torch_voltage * (1 / scale) + offset) / 1e6:.2f} MHz")
 
-    print(f"At max voltage we use: {max_torch_voltage / (volt_full * plasma_divider_ratio) * 100:.0f}% of the total")
+    print(f"At max voltage we use: {max_torch_voltage / (board['volt_full'] * board['plasma_divider_ratio']) * 100:.0f}% of the total")
+    print(f"Input voltage will be {board['volt_full']}v to give max torch voltage of {board['volt_full']*board['plasma_divider_ratio']:.0f}v")
 
     print('-' * 40 + '\n')
 

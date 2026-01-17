c7a445e2e2101877f9ca71228343c56683f7ff7c
Andy Alexander <code@homeemail.me.uk>
2026-01-17 16:38:44 +0000

    setup.sh

---
+++ docs/setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install net-tools
+sudo apt-get install git
+
+#sudo rm /etc/{initramfs/post-update.d/,kernel/{postinst.d/,postrm.d/}}z50-raspi-firmware
+#sudo apt purge raspi-firmware

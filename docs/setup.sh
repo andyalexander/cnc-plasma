e4eab528f74419154a09281cb329368ed360a541
Andy Alexander <andrew@homeemail.me.uk>
2026-05-25 19:49:38 +0100

    Update setup.sh

--- docs/setup.sh
+++ docs/setup.sh
@@ -63,3 +63,11 @@ echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc && source ~/.bashrc
 sudo apt install python3-gpg -y &&
 wget -O /tmp/dropbox.deb "https://linux.dropbox.com/packages/debian/dropbox_2026.05.06_amd64.deb" &&
 sudo dpkg -i /tmp/dropbox.deb ; sudo apt-get install -f -y
+
+# VScode
+sudo apt install wget gpg apt-transport-https -y &&
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/packages.microsoft.gpg > /dev/null &&
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] \
+  https://packages.microsoft.com/repos/code stable main" \
+  > /etc/apt/sources.list.d/vscode.list' &&
+sudo apt update && sudo apt install code -y
